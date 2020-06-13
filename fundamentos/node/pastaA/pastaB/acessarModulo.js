32b09ced3150b9a3325e40db979cde2d7ffea301
Tomas Hugo <tomas.hugo@gmail.com>
2020-06-13 13:27:43 -0300

    new change

--- fundamentos/node/pastaA/pastaB/acessarModulo.js
+++ fundamentos/node/pastaA/pastaB/acessarModulo.js
@@ -5,3 +5,14 @@ console.log(moduloA.ola);
 
 const saudacao = require("saudacao");
 console.log(saudacao.ola);
+
+const c = require("./pastaC");
+console.log(c.ola2);
+
+// const http = require("http");
+// http
+//   .createServer((req, res) => {
+//     res.write("Bom dia!");
+//     res.end();
+//   })
+//   .listen(8080);
