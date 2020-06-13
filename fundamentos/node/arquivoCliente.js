3e394e0db925793d086a6296ffcaeea2232abea3
Tomas Hugo <tomas.hugo@gmail.com>
2020-06-13 17:31:32 -0300

    exercicios de passagem de parâmetros, leitura e escrita de arquivos e exemplos do this no nodejs

---
+++ fundamentos/node/arquivoCliente.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+
+const caminho = __dirname + "/arquivo.json";
+
+// sincrono...
+const conteudo = fs.readFileSync(caminho, "utf-8");
+console.log(conteudo);
+
+// assincrono
+fs.readFile(caminho, "utf-8", (err, conteudo) => {
+  if (!err) {
+    const config = JSON.parse(conteudo);
+    console.log(`${config.db.host}:${config.db.port}`);
+  }
+});
+
+const config = require("./arquivo.json");
+console.log(config.db.user);
+
+fs.readdir(__dirname, (err, conteudo) => {
+  if (!err) {
+    console.log("Conteúdo da pasta");
+    console.log(conteudo);
+  }
+});
--- fundamentos/node/this.js
+++ fundamentos/node/this.js
@@ -3,3 +3,14 @@ console.log(this === module);
 
 console.log(this === module.exports);
 console.log(this === exports);
+
+function logThis() {
+  console.log("Dentro de uma função...");
+  console.log(this === exports);
+  console.log(this === module.exports);
+  console.log(this === global);
+
+  this.perigo = "..."; // coloca no escopo global
+}
+
+logThis();
