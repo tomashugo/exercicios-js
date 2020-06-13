2e2aa753bfea6639f3c9ee36dcdfa5cb5569da0c
Tomas Hugo <tomas.hugo@gmail.com>
2020-06-13 16:10:57 -0300

    exercícios do módulo de node

---
+++ fundamentos/node/funcionarios/funcionarios.js
@@ -0,0 +1,20 @@
+const url = "http://files.cod3r.com.br/curso-js/funcionarios.json";
+const axios = require("axios");
+
+const chineses = (func) => func.pais === "China";
+const mulheres = (func) => func.genero === "F";
+const menorSalario = (func, funcAtual) => {
+  return func.salario > funcAtual.salario ? func : funcAtual;
+};
+
+axios.get(url).then((response) => {
+  const funcionarios = response.data;
+  console.log(funcionarios);
+
+  const func = funcionarios
+    .filter(chineses)
+    .filter(mulheres)
+    .reduce(menorSalario);
+
+  console.log(func);
+});
